052b5574ded2247ba34b7ed0be1cfca3908e17d9
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-04-25 14:47:37 +0200

    add module, add scrollToTop button, add function which updates year in the footer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
-import './styles/style.scss'
+import "./styles/style.scss";
 
-$('#carousel-example').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 5;
-    var totalItems = $('.carousel-item').length;
+window.addEventListener("DOMContentLoaded", () => {
+  var module = function (element) {
+    const headerHeight = document.querySelector(".header").offsetHeight;
+    const headerPosition = headerHeight / 2;
+    const btnScroll = element;
+    const year = document.querySelector(".current-year");
 
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-            if (e.direction == "left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            } else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
+    return {
+      showBtnScroll: function () {
+        if (window.scrollY > headerPosition) {
+          btnScroll.classList.add("button-scroll--show");
+        } else {
+          btnScroll.classList.remove("button-scroll--show");
         }
-    }
+      },
+      scrollToTop: function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      },
+      updateYear: function () {
+        const today = new Date();
+        const currentYear = today.getFullYear();
+
+        year.innerHTML = currentYear;
+      },
+    };
+  };
+
+  const btnScroll = document.querySelector(".button-scroll");
+
+  var scroll = module(btnScroll);
+
+  window.addEventListener("scroll", scroll.showBtnScroll);
+  btnScroll.addEventListener("click", scroll.scrollToTop);
+  scroll.updateYear();
 });
+
+// $("#carousel-example").on("slide.bs.carousel", function (e) {
+//   var $e = $(e.relatedTarget);
+//   var idx = $e.index();
+//   var itemsPerSlide = 5;
+//   var totalItems = $(".carousel-item").length;
+
+//   if (idx >= totalItems - (itemsPerSlide - 1)) {
+//     var it = itemsPerSlide - (totalItems - idx);
+//     for (var i = 0; i < it; i++) {
+//       if (e.direction == "left") {
+//         $(".carousel-item").eq(i).appendTo(".carousel-inner");
+//       } else {
+//         $(".carousel-item").eq(0).appendTo(".carousel-inner");
+//       }
+//     }
+//   }
+// });
