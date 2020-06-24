74d14d21c48a13393d9fe455937f814b9b0bebf9
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-06-24 22:17:09 +0200

    add basic styles to the carousel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import "./styles/style.scss";
+import $ from "jquery";
+import "slick-carousel";
+
+window.jQuery = window.$ = $;
 
 window.addEventListener("DOMContentLoaded", () => {
+  $(".carousel__items").slick({
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
   // fetch(
   //   "https://asos2.p.rapidapi.com/products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US",
   //   {
